efc906902f950ec85a0aa2f49690d64732883f05
Gersón Gómez <gergomez18@gmail.com>
2021-02-11 19:23:58 -0500

    Actualización

--- core/models/Laboratory_model.php
+++ core/models/Laboratory_model.php
@@ -282,7 +282,7 @@ class Laboratory_model extends Model
 				$writing .= '<tr style="width:100%;margin:0px;padding:0px;border:0px;">';
 
 				if ($data['custody_chain']['type'] == 'covid_pcr')
-					$writing .= '<td style="width:25%;margin:0px;padding:10px 0px 10px 10px;border:0px;box-sizing:border-box;font-size:12px;font-weight:400;text-align:left;color:#004770;">PCR-SARS-CoV-2 (COVID-19) <br> ' . Languages::email('atila_biosystem')[$data['custody_chain']['lang']] . '</td>';
+					$writing .= '<td style="width:25%;margin:0px;padding:10px 0px 10px 10px;border:0px;box-sizing:border-box;font-size:12px;font-weight:400;text-align:left;color:#004770;">PCR-SARS-CoV-2 (COVID-19)</td>';
 				else if ($data['custody_chain']['type'] == 'covid_an')
 					$writing .= '<td style="width:25%;margin:0px;padding:10px 0px 10px 10px;border:0px;box-sizing:border-box;font-size:12px;font-weight:400;text-align:left;color:#004770;">Ag-SARS-CoV-2 (COVID-19)</td>';
 
@@ -291,6 +291,17 @@ class Laboratory_model extends Model
 					<td style="width:25%;margin:0px;padding:10px 0px 10px 10px;border:0px;box-sizing:border-box;font-size:12px;font-weight:400;text-align:left;color:#004770;">' . Languages::email($data['test_unity'])[$data['custody_chain']['lang']] . '</td>
 					<td style="width:25%;margin:0px;padding:10px;border:0px;box-sizing:border-box;font-size:12px;font-weight:400;text-align:left;color:#004770;">' . Languages::email($data['test_reference_values'])[$data['custody_chain']['lang']] . '</td>
 				</tr>';
+
+				if ($data['custody_chain']['type'] == 'covid_pcr')
+				{
+					$writing .=
+					'<tr style="width:100%;margin:0px;padding:0px;border:0px;">
+				        <td style="width:25%;margin:0px;padding:10px 0px 0px 10px;border:0px;box-sizing:border-box;font-size:10px;font-weight:600;text-align:left;color:#004770;">' . Languages::email('atila_biosystem')[$data['custody_chain']['lang']] . '</td>
+				        <td style="width:25%;margin:0px;padding:10px 0px 0px 10px;border:0px;box-sizing:border-box;"></td>
+				        <td style="width:25%;margin:0px;padding:10px 0px 0px 10px;border:0px;box-sizing:border-box;"></td>
+				        <td style="width:25%;margin:0px;padding:10px 10px 0px 10px;border:0px;box-sizing:border-box;"></td>
+				    </tr>';
+				}
 			}
 			else if ($data['custody_chain']['type'] == 'covid_ac')
 			{
