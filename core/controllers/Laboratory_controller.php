bf31ed30000376bc2a428976a8583aa351b4f3e8
Gersón Gómez <gergomez18@gmail.com>
2021-02-24 12:29:16 -0500

    Actualización

--- core/controllers/Laboratory_controller.php
+++ core/controllers/Laboratory_controller.php
@@ -22,6 +22,8 @@ class Laboratory_controller extends Controller
 		{
 			define('_title', Configuration::$web_page . ' | Marbu Salud');
 
+			// $this->model->sql();
+
 			$template = $this->view->render($this, 'marbu');
 
 			echo $template;
@@ -30,8 +32,6 @@ class Laboratory_controller extends Controller
 
 	public function index($params)
     {
-		// $this->model->sql();
-
         if (Format::exist_ajax_request() == true)
 		{
 			if ($_POST['action'] == 'filter_custody_chains')
