ba19939b6f5633d7b5c24bd02191b6a53df18d7f
Gersón Gómez <gergomez18@gmail.com>
2021-03-09 10:46:18 -0500

    Actualización

--- layouts/head.php
+++ layouts/head.php
@@ -16,7 +16,7 @@
 		<link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
 		<link rel="stylesheet" href="https://cdn.codemonkey.com.mx/monkeyboard/assets/css/valkyrie.min.css" type="text/css" media="all" />
 		<link rel="stylesheet" href="https://cdn.codemonkey.com.mx/monkeyboard/assets/css/styles.css" type="text/css" media="all" />
-		<link rel="stylesheet" href="{$path.css}styles.css?v=1.1" type="text/css" media="all" />
+		<link rel="stylesheet" href="{$path.css}styles.css?v=1.3" type="text/css" media="all" />
 		<link href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i" rel="stylesheet" />
 		<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
 		{$dependencies.css}
